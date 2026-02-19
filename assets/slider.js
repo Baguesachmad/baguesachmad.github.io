fb8f5f9ab71312d5b7d38dd6da535d43c85fc04f
GoSmarttools <red.mediaonlinenusantara@gmail.com>
2026-02-19 09:03:07 +0700

    assets/slider.js

--- assets/slider.js
+++ assets/slider.js
@@ -1,1 +1,10 @@
-<script src="/assets/slider.js"></script>
+let slides = document.querySelectorAll(".slide");
+let index = 0;
+
+function showSlide() {
+  slides.forEach(s => s.classList.remove("active"));
+  slides[index].classList.add("active");
+  index = (index + 1) % slides.length;
+}
+
+setInterval(showSlide, 4000);
